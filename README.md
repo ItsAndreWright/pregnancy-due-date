5f3e417b90a6e5454ab491de8c10194f08837f0f
Andre Wright <andremwright@outlook.com>
2022-05-03 22:00:07 -0400

    Complete the functions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# pregnancy-due-date
+# Pregnancy Due Date
+
+This is a Pregnancy Due Date project that I worked on to practice writing JavaScript. The user inputs the month of conception and the program returns the expected month of delivery.
--- index.js
+++ index.js
@@ -17,6 +17,30 @@ function getConceptionMonthNumber() {
         case "april":
             conceptionMonthNumber = 4;
             break;
+        case "may":
+            conceptionMonthNumber = 5;
+            break;
+        case "june":
+            conceptionMonthNumber = 6;
+            break;
+        case "july":
+            conceptionMonthNumber = 7;
+            break;
+        case "august":
+            conceptionMonthNumber = 8;
+            break;
+        case "september":
+            conceptionMonthNumber = 9;
+            break;
+        case "october":
+            conceptionMonthNumber = 10;
+            break;
+        case "november":
+            conceptionMonthNumber = 11;
+            break;
+        case "december":
+            conceptionMonthNumber = 12;
+            break;
         default:
             alert("Invalid month.");
             break;
@@ -28,6 +52,39 @@ function getDeliveryMonth() {
     switch (deliveryMonthNumber) {
         case 10:
             deliveryMonth = "October";
+            break;
+        case 11:
+            deliveryMonth = "November";
+            break;
+        case 12:
+            deliveryMonth = "December";
+            break;
+        case 13:
+            deliveryMonth = "January";
+            break;
+        case 14:
+            deliveryMonth = "February";
+            break;
+        case 15:
+            deliveryMonth = "March";
+            break;
+        case 16:
+            deliveryMonth = "April";
+            break;
+        case 17:
+            deliveryMonth = "May";
+            break;
+        case 18:
+            deliveryMonth = "June";
+            break;
+        case 19:
+            deliveryMonth = "July";
+            break;
+        case 20:
+            deliveryMonth = "August";
+            break;
+        case 21:
+            deliveryMonth = "September";
             break;
         default:
             break;
